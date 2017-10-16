37325a21ab1e9265070ea6db1063056dfad661fb
Pablo Pesce <papesce@gmail.com>
2017-10-16 20:59:37 -0300

    fix tests

--- src/index.js
+++ src/index.js
@@ -8,8 +8,18 @@ process.on('unhandledRejection', (reason, p) =>
   logger.error('Unhandled Rejection at: Promise ', p, reason)
 );
 
-server.on('listening', () =>
-  logger.info('Feathers application started on http://%s:%d', app.get('host'), port)
-);
+server.on('listening', () => {
+  logger.info('Feathers application started on http://%s:%d', app.get('host'), port);
+  const routineService = app.service('routine');
+  
+
+
+  routineService.get('59e4c109c1d9dcfb4e7c31b9').then( 
+      (res) => {
+        logger.info('resource found');
+      }
+    );
+
+});
 
 
--- test/services/exercise.test.js
+++ test/services/exercise.test.js
@@ -4,7 +4,25 @@ const app = require('../../src/app');
 describe('\'exercise\' service', () => {
   it('registered the service', () => {
     const service = app.service('exercise');
-
     assert.ok(service, 'Registered the service');
+  });
+
+  it('should create a model', () => {
+    //const exerciseService = app.service('exercise');
+    // exerciseService.create({name:'Bench Press'});
+  }); 
+
+  it('should connect routine with excercise', (done) => {
+    //const exerciseService = app.service('exercise');
+    //const ex = exerciseService.get('59e53f1622f6d840c6c3359e');
+    //assert.ok(ex, 'Found the exercise');  
+    const routineService = app.service('routine');
+    routineService.get('59e4c109c1d9dcfb4e7c31b9').then( 
+      (res) => {
+        assert.ok(res, 'Found the routine'); 
+        done();
+      }
+    );
+    
   });
 });
